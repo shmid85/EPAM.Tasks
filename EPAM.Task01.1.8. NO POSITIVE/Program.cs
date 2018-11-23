545f2683ee3f86d3b6fdc14b1601d4d3506ffd78
Александр Мартынюк <shmid1985@outlook.com>
2018-11-23 23:12:25 +0400

    Add 1.8. Dont;t finish it.

---
+++ EPAM.Task01.1.8. NO POSITIVE/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPAM.Task01._1._8.NO_POSITIVE
+{
+    class Program
+    {
+        static int n=0;
+        static int[,,] array;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            AskArrayLength();
+            array = new int[n, n, n];
+            GererateArray(n);
+            PrintArray();
+
+
+            Console.ReadLine();
+        }
+
+        public static int AskArrayLength()
+        {
+            Console.Write("Введите количество элементов массива: ");
+            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0 || n > 100)
+            {
+                Console.Write("Введите количество элементов массива: ");
+                if (n > 100) Console.WriteLine("\nСлишком много элементов. Попробуйте менее 100...");
+            }
+
+            return n;
+        }
+
+        public static int[,,] GererateArray(int n)
+        {
+            Random random = new Random();
+            
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                        array[i, j, k] = random.Next(-2*n, 2*n);
+            return array;
+        }
+
+        public static void PrintArray() {
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                        Console.WriteLine(array[i, j, k]);
+
+        }
+
+    }
+}
